e1c36947b667ab5dcd40c900cd011aa76da697b2
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2024-05-05 15:59:06 +0200

    atelier1/dynamic(refactoring): refactoring des exceptions et services, correction annotations DAO

--- atelier1/dynamic/src/main/java/cpe/atelier1/services/CardService.java
+++ atelier1/dynamic/src/main/java/cpe/atelier1/services/CardService.java
@@ -22,40 +22,12 @@ import java.util.Optional;
 @Service
 public class CardService {
 
-    Logger logger = LoggerFactory.getLogger(CardService.class);
 
     @Autowired
     CardDao cardDao;
 
     public List<Card> getCards() {
-        ArrayList<Card> cards = new ArrayList<>();
-        CardSerializer<ObjectNode> serializer = new CardJSONSerializer();
-        InputStream JSONStream  = getClass().getClassLoader()
-                .getResourceAsStream("cards.json");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = null;
-        // TODO: utiliser l'injection de dépendance pour charger les cartes depuis le JSON (DAO)
-        try {
-            node = mapper.readTree(JSONStream);
-        } catch (IOException e) {
-            logger.error("An error occured while reading cards file, returning empty array", e);
-        }
-
-        assert node != null;
-        if (! node.isArray()) {
-            logger.error("Expected array from cards json but got {}", node.getNodeType().toString());
-            return cards;
-        }
-
-        node.forEach(jsonNode -> {
-            if (! jsonNode.isObject()) {
-                logger.error("Expected object from cards json but got {}", jsonNode.getNodeType().toString());
-                return;
-            }
-            cards.add(serializer.deserialize((ObjectNode) jsonNode));
-        });
-
-        return cards;
+        return cardDao.getCardList();
     }
 
     public Card getCard(int id) {
@@ -71,46 +43,8 @@ public class CardService {
                 cardForm.getAffinity(), cardForm.getImgUrl(), cardForm.getSmallImgUrl(),
                 cardForm.getEnergy(), cardForm.getHp(), cardForm.getDefence(), cardForm.getAttack(),
                 cardForm.getPrice(), cardForm.getUserId());
-        CardSerializer<ObjectNode> serializer = new CardJSONSerializer();
-        // TODO: Utiliser l'injection de dépendance pour sauvegarder les cartes (DAO)
-        InputStream JSONStream  = getClass().getClassLoader()
-                .getResourceAsStream("cards.json");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = null;
-
-        try {
-            node = mapper.readTree(JSONStream);
-        } catch (IOException e) {
-            logger.error("An error occured while reading cards file, returning empty array", e);
-        }
-        assert node != null;
-
-        if (! node.isArray()) {
-            logger.error("Expected array from cards json but got {}", node.getNodeType().toString());
-            return Optional.of(new Exception("Expected array from cards json but got " + node.getNodeType().toString()));
-        }
-
-        ArrayNode array = (ArrayNode) node;
-        array.add(serializer.serialize(card));
-
-
-        try {
-            writeToJsonFile(array);
-        } catch (IOException e) {
-            logger.error("An error occured while writing json file", e);
-            return Optional.of(new Exception("An error occured while writing card to persistent storage, please contact the administrator"));
-        }
-
-        return Optional.empty();
+        return cardDao.addCard(card);
     }
 
-    private void writeToJsonFile(JsonNode node) throws IOException {
-        String JSONPath = getClass().getClassLoader()
-                .getResource("cards.json").getPath();
-        File JSONFile = new File(JSONPath);
-        FileWriter fileWriter = new FileWriter(JSONFile);
-        fileWriter.write(node.toString());
 
-        fileWriter.close();
-    }
 }
