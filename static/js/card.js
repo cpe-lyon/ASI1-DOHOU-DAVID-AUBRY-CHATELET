d97e8ea6de9fe9cbc05d931781552784b222091e
GTK188 <axoloto188@gmail.com>
2024-05-22 16:12:28 +0200

    Edited card list

--- static/js/card.js
+++ static/js/card.js
@@ -1,34 +1,4 @@
-let _cardlist = [
-        {
-            family_src:"https://imgc.allpostersimages.com/img/print/affiches/marvel-super-hero-squad-iron-man-standing_a-G-9448041-4985690.jpg",
-            family_name:"Jose",
-            image_src:"https://imgc.allpostersimages.com/img/print/affiches/marvel-super-hero-squad-iron-man-standing_a-G-9448041-4985690.jpg",
-            date:"14h",
-            comment:"3 comments",
-            like: "17 likes",
-            button: "Buy"
-        },
-        {
-            family_src:"https://media.giphy.com/media/l4q8hciiYNT5RGi4w/giphy.gif",
-            family_name:"John",
-            image_src:"https://media.giphy.com/media/l4q8hciiYNT5RGi4w/giphy.gif",
-            date:"1h",
-            comment:"345 comments",
-            like: "1000 likes",
-            button: "Read"
-        },
-        {
-            family_src:"http://www.superherobroadband.com/app/themes/superhero/assets/img/superhero.gif",
-            family_name:"Elliot",
-            image_src:"http://www.superherobroadband.com/app/themes/superhero/assets/img/superhero.gif",
-            date:"142h",
-            comment:"0 comment",
-            like: "1 likes",
-            button: "Read"
-        }
-
-    ];
-
+//https://localhost:8000/card/all
 let cards = fetch("http://tp.cpe.fr:8083/cards").then(value => {
     if (! value.ok) {
         alert(`Erreur lors de la récupération des cartes : ${value.status}`)
@@ -42,6 +12,7 @@ function loadCards(json) {
     let template = document.querySelector("#selectedCard");
 
     for(const card of json){
+        console.log(card)
         let clone = document.importNode(template.content, true);
 
         newContent= clone.firstElementChild.innerHTML
