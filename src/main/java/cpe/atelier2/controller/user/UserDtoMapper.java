45911e86a6c67d68232dc2ac8709855eb7317ef9
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2024-05-26 22:18:53 +0200

    atelier2/front: correction frontend

--- src/main/java/cpe/atelier2/controller/user/UserDtoMapper.java
+++ src/main/java/cpe/atelier2/controller/user/UserDtoMapper.java
@@ -1,6 +1,8 @@
 package cpe.atelier2.controller.user;
 
+import cpe.atelier2.controller.card.CardDtoMapper;
 import cpe.atelier2.domain.user.User;
+import cpe.atelier2.repository.card.CardEntityMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,8 +10,18 @@ import java.util.ArrayList;
 @Component
 public class UserDtoMapper {
 
+    private final CardDtoMapper cardDtoMapper;
+    private final CardEntityMapper cardEntityMapper;
+
+    public UserDtoMapper(CardDtoMapper cardDtoMapper, CardEntityMapper cardEntityMapper) {
+        this.cardDtoMapper = cardDtoMapper;
+        this.cardEntityMapper = cardEntityMapper;
+    }
+
     public UserDTO userToUserDto(User user) {
-        return new UserDTO(user.id(), user.email(), user.username(), user.password(), user.money());
+        return new UserDTO(user.id(), user.email(), user.username(), user.password(), user.money(), user.cardList().stream().map(cardEntity -> {
+            return cardDtoMapper.cardToCardDTO(cardEntityMapper.cardEntityToCard(cardEntity));
+        }).toList());
     }
 
     public User userFormDtoToUser(UserFormDTO userDTO) {
