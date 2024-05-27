152b8fbbe1384eb3c0cbbdfa500a61a239bcdfd2
GTK188 <axoloto188@gmail.com>
2024-05-27 16:42:48 +0200

    Added get cards on sale

--- static/js/sell.js
+++ static/js/sell.js
@@ -1,27 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchCards();
-});
-
-function fetchCards() {
-    fetch('http://localhost:8090/market/all')
-        .then(response => response.json())
-        .then(cards => {
-            const tableBody = document.getElementById('table-body');
-            cards.forEach(card => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${card.nom}</td>
-                    <td>${card.description}</td>
-                    <td>${card.famille}</td>
-                    <td>${card.affinite}</td>
-                    <td>${card.energie}</td>
-                    <td>${card.hp}</td>
-                    <td>${card.prix}$</td>
-                `;
-                tableBody.appendChild(row);
-            });
-        })
-        .catch(error => {
-            console.error('Error loading the cards:', error);
-        });
+function getMarketCards() {
+    let cards = fetch("http://localhost:8090/market/all").then(value => {
+        if (! value.ok) {
+            alert(`Erreur lors de la récupération des cartes : ${value.status}`)
+            return
+        }
+        value.json().then(json => loadMarketCards(json))
+    })
 }
+
+function loadMarketCards(json) {
+    let template = document.querySelector("#bodyTemplate")
+
+    for(const el of json){
+        console.log(el)
+        let clone = document.importNode(template.content, true)
+
+        newContent= clone.firstElementChild.innerHTML
+            .replace(/{{card_name}}/g, el.card.name)
+            .replace(/{{card_description}}/g, el.card.description)
+            .replace(/{{card_family}}/g, el.card.family)
+            .replace(/{{card_affinity}}/g, el.card.affinity)
+            .replace(/{{card_imgUrl}}/g, el.card.imgUrl)
+            .replace(/{{card_smallImgUrl}}/g, el.card.smallImgUrl)
+            .replace(/{{card_energy}}/g, el.card.energy)
+            .replace(/{{card_hp}}/g, el.card.hp)
+            .replace(/{{card_defence}}/g, el.card.defence)
+            .replace(/{{card_attack}}/g, el.card.attack)
+            .replace(/{{card_price}}/g, el.price)
+        clone.firstElementChild.innerHTML= newContent
+
+        let cardContainer= document.querySelector("#table-body")
+        cardContainer.appendChild(clone)
+    }
+
+    document.querySelectorAll('.card-item').forEach(card => {
+        card.addEventListener('click', function() {
+            this.classList.toggle('flipped');
+        });
+    });
+}
+
+$(document).ready(function() {
+    getMarketCards()
+})
