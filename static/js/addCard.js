4b9ed31596e829a4cd24fc46d622c890bdafb9e3
GTK188 <axoloto188@gmail.com>
2024-05-27 11:58:58 +0200

    Reworked addCard.js & card.js

--- static/js/addCard.js
+++ static/js/addCard.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
     $('.ui.form').submit(function(event) {
-        event.preventDefault(); // Empêche le comportement par défaut de soumission du formulaire
+        event.preventDefault()
 
-        let formData = {};
-        let isFormValid = true;
+        let formData = {}
+        let isFormValid = true
 
         $(this).find('input').each(function() {
-            const name = $(this).attr('name').replace('card-', ''); // Simplifie le nom de l'attribut pour correspondre à l'API
-            const value = $(this).val().trim();
+            const name = $(this).attr('name').replace('card-', '')
+            const value = $(this).val().trim()
 
             if (value === '') {
-                alert('Veuillez remplir tous les champs.');
-                isFormValid = false;
-                return false; // Quitte la boucle .each
+                alert('Veuillez remplir tous les champs.')
+                isFormValid = false
+                return false
             }
-            formData[name] = $(this).attr('type') === 'number' ? parseInt(value, 10) : value;
-        });
+            formData[name] = $(this).attr('type') === 'number' ? parseInt(value, 10) : value
+        })
 
         if (!isFormValid) {
-            return; // Stoppe la fonction si le formulaire n'est pas valide
+            return
         }
 
-        // Envoi de la requête AJAX
         $.ajax({
             url: 'http://localhost:8090/card/add',
             type: 'POST',
@@ -32,18 +31,8 @@ $(document).ready(function() {
                 window.location = './card-list'
             },
             error: function(xhr, status, error) {
-                alert('Erreur lors de la soumission des données: ' + error);
+                alert('Erreur lors de la soumission des données: ' + error)
             }
-        });
-        // $.post("http://localhost:8090/card/add",
-        // JSON.stringify(formData),
-        // function(data, status){
-        //     alert("Data: " + data + "\nStatus: " + status);
-        // },'json').done(function( response ) {
-        //     alert('Données soumises avec succès.')
-        //     window.location = '/card-list'
-        // }).fail(function (xhr, status, error){
-        //     alert('Erreur lors de la soumission des données: ' + error)
-        // });
-    });
-});
+        })
+    })
+})
--- static/js/card.js
+++ static/js/card.js
@@ -1,33 +1,49 @@
-//http://localhost:8090/card/all
-//http://tp.cpe.fr:8083/cards
-let cards = fetch("http://localhost:8090/card/all").then(value => {
-    if (! value.ok) {
-        alert(`Erreur lors de la récupération des cartes : ${value.status}`)
-        return
-    }
-    value.json().then(json => loadCards(json))
-})
+function getAllCards(){
+    let cards = fetch("http://localhost:8090/card/all").then(value => {
+        if (! value.ok) {
+            alert(`Erreur lors de la récupération des cartes : ${value.status}`)
+            return
+        }
+        value.json().then(json => loadCards(json))
+    })
+}
 
+function getCardById(id){
+    let card = fetch("http://localhost:8090/card/"+id).then(value => {
+        if (! value.ok) {
+            alert(`Erreur lors de la récupération de la carte : ${value.status}`)
+            return
+        }
+        value.json().then(json => loadCards(json))
+    })
+}
 
 function loadCards(json) {
-    let template = document.querySelector("#selectedCard");
+    let template = document.querySelector("#selectedCard")
 
     for(const card of json){
         console.log(card)
-        let clone = document.importNode(template.content, true);
+        let clone = document.importNode(template.content, true)
 
         newContent= clone.firstElementChild.innerHTML
-            .replace(/{{card_small_img}}/g, card.smallImgUrl)
+            .replace(/{{card_id}}/g, card.id)
             .replace(/{{card_name}}/g, card.name)
-            .replace(/{{card_img}}/g, card.imgUrl)
-            .replace(/{{card_id}}/g, card.id);
-        clone.firstElementChild.innerHTML= newContent;
+            .replace(/{{card_description}}/g, card.description)
+            .replace(/{{card_family}}/g, card.family)
+            .replace(/{{card_affinity}}/g, card.affinity)
+            .replace(/{{card_imgUrl}}/g, card.imgUrl)
+            .replace(/{{card_smallImgUrl}}/g, card.smallImgUrl)
+            .replace(/{{card_energy}}/g, card.energy)
+            .replace(/{{card_hp}}/g, card.hp)
+            .replace(/{{card_defence}}/g, card.defence)
+            .replace(/{{card_attack}}/g, card.attack)
+        clone.firstElementChild.innerHTML= newContent
 
-        let cardContainer= document.querySelector("#gridContainer");
-        cardContainer.appendChild(clone);
+        let cardContainer= document.querySelector("#gridContainer")
+        cardContainer.appendChild(clone)
     }
 }
 
-function showCardDetails(id) {
-    location.href = `./details.html?id=${id}`
-}
+// function showCardDetails(id) {
+//     location.href = `./details.html?id=${id}`
+// }
