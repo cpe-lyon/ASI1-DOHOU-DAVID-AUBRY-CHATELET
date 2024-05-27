b21d85e7722253bcd2c02c3a7defccac6155426f
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2024-05-27 10:20:41 +0200

    atelier 3 : ajout cartes utilisateur a l'enregistrement

--- src/main/java/cpe/atelier2/repository/user/UserEntityMapper.java
+++ src/main/java/cpe/atelier2/repository/user/UserEntityMapper.java
@@ -1,14 +1,24 @@
 package cpe.atelier2.repository.user;
 
 import cpe.atelier2.domain.user.User;
+import cpe.atelier2.repository.card.CardEntityMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserEntityMapper {
+    private final CardEntityMapper cardEntityMapper;
+
+    public UserEntityMapper(CardEntityMapper cardEntityMapper) {
+        this.cardEntityMapper = cardEntityMapper;
+    }
 
     public User userEntityToUser(UserEntity userEntity) {
         return new User(userEntity.getId(), userEntity.getEmail(),
-                userEntity.getUsername(), userEntity.getPassword(), userEntity.getMoney(), userEntity.getCardEntityList());
+                userEntity.getUsername(), userEntity.getPassword(), userEntity.getMoney(),
+                userEntity.getCardEntityList().stream()
+                        .map(cardEntityMapper::cardEntityToCard)
+                        .toList()
+        );
     }
 
     public UserEntity userToUserEntity(User user){
@@ -19,6 +29,10 @@ public class UserEntityMapper {
         userEntity.setUsername(user.username());
         userEntity.setMoney(user.money());
         userEntity.setPassword(user.password());
+        userEntity.setCardEntityList(user.cardList().stream()
+                .map(cardEntityMapper::cardToCardEntity)
+                .toList()
+        );
 
         return userEntity;
     }
