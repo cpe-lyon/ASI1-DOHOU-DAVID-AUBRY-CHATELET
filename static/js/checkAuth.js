f0593bda3968629648f689c4f1b28132b7874cd5
GTK188 <axoloto188@gmail.com>
2024-05-28 14:48:10 +0200

    Login + check auth done

---
+++ static/js/checkAuth.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'http://localhost:8090/auth/check',
+        type: 'POST',
+        contentType: 'application/json',
+        xhrFields: {withCredentials: true},
+        crossDomain: true,
+        success: function (response) {
+            console.log('check GOOD: ' + response)
+        },
+        error: function (xhr, status, error) {
+            alert('Vous n\'êtes pas autorisé à accéder à ce contenu \n Veuillez vous connecter')
+            window.location = './login'
+        }
+    })
+})
